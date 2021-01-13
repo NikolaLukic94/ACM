e3d3e4d9f6bcb26aa6ff26d4a53276af51dd43c3
unknown <nikola94lukic@hotmail.com>
2021-01-13 09:36:46 +0100

    customer class and tests

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace ACM.BL
 {
-    public class Class1
+    public class Customer
     {
+        // shortcut is propg + tab + tab for creating getter w/o setter
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+
+        public static int InstanceCount { get; set; } // shares with all instances
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
     }
 }
